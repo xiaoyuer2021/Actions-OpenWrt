25e4514b0792171c996742c0872e64b2d6f26971
luojun853 <as853472853@gmail.com>
2020-07-27 00:24:29 +0800

    Add diy packages

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,15 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+rm -rf ./package/lean/luci-theme-argon
+
+git clone https://github.com/jerrykuku/luci-theme-argon /package/luci-theme-argon
+
+git clone https://github.com/kevin-morgan/luci-theme-argon-dark /package/luci-theme-argon-dark
+
+git clone https://github.com/tty228/luci-app-serverchan /package/luci-app-serverchan
+
+git clone https://github.com/jerrykuku/node-request /package/node-request
+
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus /package/luci-app-jd-dailybonus
